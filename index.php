fa57097b6fe5b7615538a6c7ee7cf8c5ac17c237
korisu07 <Hallo.Dreamy@gmail.com>
2020-11-04 15:37:10 +0900

    下準備。送信したメッセージをページ内に表示できるようにしました。

--- index.php
+++ index.php
@@ -3,9 +3,16 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+  <title>チャットツール</title>
 </head>
 <body>
-  
+  <article>
+    <form action="index.php" method="post">
+      <input type="text" name="chat" id="chat">
+      <button>送信</button>
+    </form>
+
+    <?php echo $_POST['chat'] ?>
+  </article>
 </body>
 </html>
