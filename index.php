30c0fc4b68c7842f320182bd01f8957dcca82327
korisu07 <Hallo.Dreamy@gmail.com>
2020-11-16 21:07:16 +0900

    ユーザー名とメッセージを入力する欄を分けました。

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,11 +8,16 @@
 <body>
   <article>
     <form action="/" method="post">
-      <input type="text" name="chat" id="chat">
+      <label for="user_name">ユーザー名：</label>
+      <input type="text" name="user_name" id="chat">
+
+      <label for="chat_message">メッセージ：</label>
+      <input type="text" name="chat_message" id="chat">
       <button>送信</button>
     </form>
 
     <?php require_once dirname(__FILE__) . '/inc/function.php' ?>
+    
 
   </article>
 </body>
