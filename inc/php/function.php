7e62e711aedde78c872adc12d24761f4df0b56da
korisu07 <Hallo.Dreamy@gmail.com>
2020-11-17 21:06:22 +0900

    user名がCookieで保存され、入退出の処理が擬似的に行われるようにしました。他、分岐を追加

--- inc/php/function.php
+++ inc/php/function.php
@@ -17,33 +17,57 @@
     }
 
 
-    // チャットが表示された時の処理
+    // チャットが送信された時の処理
+
     if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+      // 名前が登録されていない場合
+      if(!isset($_COOKIE['Your_name'])){
+        // 未入力の場合
+        if(is_null($_POST['user_name']) || $_POST['user_name'] === ''){
+          echo 'ユーザー名を入力してください';
+          return false;
+        }//名前が入力された場合
+        else if($_POST['user_name']){
+          header('Location: /', 307);
+          setcookie('Your_name', $_POST['user_name'], time()+3600);
+        }
+      }
       
-      if(is_null($_POST['user_name']) || $_POST['user_name'] === ''){
-        die('ユーザー名を入力してください');
+      // 名前が登録されている場合
+      else if(isset($_COOKIE['Your_name'])){
+
+        // 退出ボタンを押した場合
+        if($_POST['chat_exit']){
+          header('Location: /', 307);
+          setcookie('Your_name', '', time() - 3600);
+          echo 'チャットを退出しました。';
+        }
+        // 発言内容がなにも入っていない場合
+        else if(is_null($_POST['chat_message']) || $_POST['chat_message'] === ''){
+          echo '内容が入力されていません';
+          return false;
+        }// 発言された場合
+        else{
+          header('Location: /', 307);
+  
+          $user_name = null;
+          $chat_message = null;
+    
+          $statement = $pdo->prepare('INSERT INTO chat_logs(user_name, message) VALUES(:user_name, :chat_message)');
+    
+          $user_name = $_COOKIE['Your_name'];
+          $chat_message = $_POST['chat_message'];
+    
+          $statement->bindValue(':user_name', $user_name, PDO::PARAM_STR);
+          $statement->bindValue(':chat_message', $chat_message, PDO::PARAM_STR);
+    
+          $statement->execute();
+    
+          unset($statement);
+          exit;
+        }
       }
-      if(is_null($_POST['chat_message']) || $_POST['chat_message'] === ''){
-        die('内容が入力されていません');
-      }
-
-      header('Location: /', 307);
-
-      $user_name = null;
-      $chat_message = null;
-
-      $statement = $pdo->prepare('INSERT INTO chat_logs(user_name, message) VALUES(:user_name, :chat_message)');
-
-      $user_name = $_POST['user_name'];
-      $chat_message = $_POST['chat_message'];
-
-      $statement->bindValue(':user_name', $user_name, PDO::PARAM_STR);
-      $statement->bindValue(':chat_message', $chat_message, PDO::PARAM_STR);
-
-      $statement->execute();
-
-      unset($statement);
-      exit;
     }
 
     ?>
