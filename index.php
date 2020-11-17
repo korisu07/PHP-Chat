c8b5402a6a761fa80c3135c6848e7496c934e7ba
korisu07 <Hallo.Dreamy@gmail.com>
2020-11-17 18:40:14 +0900

    チャットログを表示する処理を作りました。

--- index.php
+++ index.php
@@ -4,20 +4,46 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>チャットツール</title>
+
+  <link rel="stylesheet" href="inc/css/style.min.css">
 </head>
 <body>
+  <header>
+    <div class="container">
+
+      <form action="/" method="post">
+        <div class="header_box">
+          <label for="user_name">ユーザー名：</label>
+          <input type="text" name="user_name" id="chat">
+        </div>
+
+        <div class="header_box">
+          <label for="chat_message">メッセージ：</label>
+          <input type="text" name="chat_message" id="chat">
+        </div>
+
+        <button>発言する</button>
+      </form>
+
+    </div>
+  </header>
+  
   <article>
-    <form action="/" method="post">
-      <label for="user_name">ユーザー名：</label>
-      <input type="text" name="user_name" id="chat">
+    <div class="container">
 
-      <label for="chat_message">メッセージ：</label>
-      <input type="text" name="chat_message" id="chat">
-      <button>送信</button>
-    </form>
+      <?php require_once dirname(__FILE__) . '/inc/php/function.php' ?>
+      
+      <div class="chat_logs_view">
+        <?php while ($log_value = $access_process->fetch(PDO::FETCH_ASSOC)): ?>
+        <ul>
+          <li><?= escape($log_value['user_name']); ?></li>
+          <li><?= escape($log_value['message']); ?></li>
+          <li><?= escape($log_value['date']); ?></li>
+        </ul>
+        <?php endwhile; ?>
+      </div>
 
-    <?php require_once dirname(__FILE__) . '/inc/function.php' ?>
-    
+    </div>
 
   </article>
 </body>
