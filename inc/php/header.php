8e906a4de96fdc30153edb553a712f537d3ce607
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-02 15:37:23 +0900

    うまくいかなかったため、ユーザーの一覧表示機能を一旦廃止。

--- inc/php/header.php
+++ inc/php/header.php
@@ -35,20 +35,24 @@
 
     <?php 
 
-      try{
-        // ログイン中のユーザーを表示
-        $user_list = $pdo->prepare('SELECT login_user FROM login_user');
-        $user_list->execute();
-      } catch(PDOException $e){
-        echo 'ユーザー 一覧の表示に失敗しました。';
-      }
+    // if(isset($_SESSION)){
+    //   $this->session->all_userdata();
+    // }
+
+    // try{
+    //   // ログイン中のユーザーを表示
+    //   $user_list = $pdo->prepare('SELECT login_user FROM login_user');
+    //   $user_list->execute();
+    // } catch(PDOException $e){
+    //   echo 'ユーザー 一覧の表示に失敗しました。';
+    // }
 
     ?>
 
-    <span>入室中：</span>
-    <?php while ($log_value = $user_list->fetch(PDO::FETCH_ASSOC)): ?>
-      <span><?= text_escape($log_value['login_user']);?> さん ★ </span>
-    <?php endwhile; ?>
+    <!-- <span>入室中：</span> -->
+    <?php // while ($log_value = $user_list->fetch(PDO::FETCH_ASSOC)): ?>
+      <!-- <span><?= text_escape($log_value['login_user']);?> さん ★ </span> -->
+    <?php  // endwhile; ?>
 
     <?php
       if(isset($_COOKIE[session_name()])){
