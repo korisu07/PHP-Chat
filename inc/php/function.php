7fdb85341ae04bf34264665dbf5c5892570b0876
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-04 18:46:57 +0900

    エラー部分を修正。

--- inc/php/function.php
+++ inc/php/function.php
@@ -4,21 +4,27 @@
     return htmlspecialchars(strval($text_value), ENT_QUOTES | ENT_HTML5, 'UTF-8');
   }
 
-  $session_pattern_A = [
-    'name' => $_SESSION['data']['name'],
-    'random_id' => $_SESSION['data']['random_id'],
-    'time_stamp' => $_SESSION['data']['time_stamp'],
-    'error_message' => '',
-    'reload_time_stamp' => $reload_time_stamp,
-    'reload_count' => 0
-  ];
+  if(isset($_SESSION['data'])){
 
-    $session_pattern_B = [
-      'name' => '',
-      'random_id' => '',
-      'time_stamp' => '',
+    $reload_time_stamp = $_SERVER['REQUEST_TIME'];
+
+    $session_pattern_A = [
+      'name' => $_SESSION['data']['name'],
+      'random_id' => $_SESSION['data']['random_id'],
+      'time_stamp' => $_SESSION['data']['time_stamp'],
       'error_message' => '',
       'reload_time_stamp' => $reload_time_stamp,
       'reload_count' => 0
     ];
+  
+      $session_pattern_B = [
+        'name' => '',
+        'random_id' => '',
+        'time_stamp' => '',
+        'error_message' => '',
+        'reload_time_stamp' => $reload_time_stamp,
+        'reload_count' => 0
+      ];
+  }
+
 ?>
