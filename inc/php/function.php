74351961939f2c3a529d07c03a1b947f9778a82e
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-04 18:41:41 +0900

    一旦、セッションのあれこれを変数化

--- inc/php/function.php
+++ inc/php/function.php
@@ -4,4 +4,21 @@
     return htmlspecialchars(strval($text_value), ENT_QUOTES | ENT_HTML5, 'UTF-8');
   }
 
+  $session_pattern_A = [
+    'name' => $_SESSION['data']['name'],
+    'random_id' => $_SESSION['data']['random_id'],
+    'time_stamp' => $_SESSION['data']['time_stamp'],
+    'error_message' => '',
+    'reload_time_stamp' => $reload_time_stamp,
+    'reload_count' => 0
+  ];
+
+    $session_pattern_B = [
+      'name' => '',
+      'random_id' => '',
+      'time_stamp' => '',
+      'error_message' => '',
+      'reload_time_stamp' => $reload_time_stamp,
+      'reload_count' => 0
+    ];
 ?>
