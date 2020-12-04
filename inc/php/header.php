6766f6c6fc75abfa5d4eb73707f1eddf7c0c4fd4
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-04 18:41:55 +0900

    条件式を見直し

--- inc/php/header.php
+++ inc/php/header.php
@@ -1,30 +1,39 @@
 
   <?php include dirname(__FILE__) . '/connect/connect.php'; ?>
 
-  <form action="/" method="post" class="chat_post">
+  <?php 
 
-    <?php 
-    
+  echo var_dump($_SESSION['data']);
+  
+    if(isset($_COOKIE[session_name()]['error_message']) && $_SESSION['data']['error_message'] != ''){
+      echo '<p>' . $_SESSION['data']['error_message'] . '</p>';
+    }
+
+
     // 名前が登録されていない場合
-    if( isset($_SESSION['data']['name']) != true){
+    if( $_SESSION['data']['name'] === ''){
       echo '
+      <form action="/routing/user_name_routing.php" method="post" class="chat_post">
       <div class="header_box">
         <label for="user_name">ユーザー名：</label>
         <input type="text" name="user_name" id="chat" maxlength="20">
       </div>
 
       <input type="submit" value="入室する">
+      </from>
       ';
     }
     // 名前が登録されている場合
     else{
-      echo 
-      '<div class="header_box">
+      echo '
+      <form action="/routing/user_message_routing.php" method="post" class="chat_post">
+      <div class="header_box">
         <label for="chat_message">メッセージ：</label>
         <input type="text" name="chat_message" id="chat" maxlength="200">
       </div>
 
       <input type="submit" value="発言する">
+      </from>
       ';
     }
     
@@ -55,11 +64,11 @@
     <?php  // endwhile; ?>
 
     <?php
-      if(isset($_SESSION['data']['name'])){
+      if( $_SESSION['data']['name'] != '' ){
 
         echo '<p>' . htmlspecialchars($_SESSION['data']['name']) . '　さんとして入室中</p>' .
       
-        '<form action="/" method="post">
+        '<form action="/routing/chat_exit.php" method="post">
           <input type="submit" name="chat_exit" id="chat_exit" value="退室する">
         </form>';
       }
