57e52852fd7cdc71b9df6ef8790d27fdee0c3a36
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-05 11:51:02 +0900

    session内の配列が変数化できなかったので、削除。

--- inc/php/function.php
+++ inc/php/function.php
@@ -4,27 +4,6 @@
     return htmlspecialchars(strval($text_value), ENT_QUOTES | ENT_HTML5, 'UTF-8');
   }
 
-  if(isset($_SESSION['data'])){
-
-    $reload_time_stamp = $_SERVER['REQUEST_TIME'];
-
-    $session_pattern_A = [
-      'name' => $_SESSION['data']['name'],
-      'random_id' => $_SESSION['data']['random_id'],
-      'time_stamp' => $_SESSION['data']['time_stamp'],
-      'error_message' => '',
-      'reload_time_stamp' => $reload_time_stamp,
-      'reload_count' => 0
-    ];
-  
-      $session_pattern_B = [
-        'name' => '',
-        'random_id' => '',
-        'time_stamp' => '',
-        'error_message' => '',
-        'reload_time_stamp' => $reload_time_stamp,
-        'reload_count' => 0
-      ];
-  }
+  $reload_time_stamp = $_SERVER['REQUEST_TIME'];
 
 ?>
