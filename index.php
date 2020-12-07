7a77978c024151745e6e2db3c7e6bb47ff860b45
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-07 13:56:32 +0900

    パス指定のミスを修正

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ session_start();
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>チャットツール</title>
 
-  <link rel="stylesheet" href="inc/css/style.min.css">
+  <link rel="stylesheet" href="./inc/css/style.min.css">
 </head>
 <body>
 
