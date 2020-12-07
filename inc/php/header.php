f50855925ad8abc1813c1e30d527502c672eb046
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-07 18:09:04 +0900

    formのパス指定ミスを修正。echo文を記述を改良。ドットでつないでいたものを、カンマに変更。

--- inc/php/header.php
+++ inc/php/header.php
@@ -6,7 +6,7 @@
     // 名前が登録されている場合
     if( isset($_SESSION['data']['name']) && $_SESSION['data']['name'] != '' ){
       echo '
-      <form action="/routing/user_message_routing.php" method="post" class="chat_post">
+      <form action="./routing/user_message_routing.php" method="post" class="chat_post">
       <div class="header_box">
         <label for="chat_message">メッセージ：</label>
         <input type="text" name="chat_message" id="chat" maxlength="200">
@@ -19,7 +19,7 @@
     // 名前が登録されていない場合
     else{
       echo '
-      <form action="/routing/user_name_routing.php" method="post" class="chat_post">
+      <form action="./routing/user_name_routing.php" method="post" class="chat_post">
       <div class="header_box">
         <label for="user_name">ユーザー名：</label>
         <input type="text" name="user_name" id="chat" maxlength="20">
@@ -58,7 +58,7 @@
     <?php
       if( isset($_SESSION['data']['name']) && $_SESSION['data']['name'] != '' ){
 
-        echo '<p>' . htmlspecialchars($_SESSION['data']['name']) . '　さんとして入室中</p>' .
+        echo '<p>' , htmlspecialchars($_SESSION['data']['name']) , '　さんとして入室中</p>' ,
       
         '<form action="/routing/chat_exit.php" method="post">
           <input type="submit" name="chat_exit" id="chat_exit" value="退室する">
