288a113680db671b8ea8b8d12341a343b150b157
korisu07 <Hallo.Dreamy@gmail.com>
2020-12-07 18:09:37 +0900

    不要な処理を削除。

--- inc/php/routing.php
+++ inc/php/routing.php
@@ -10,27 +10,4 @@
       echo '<p>チャットログの表示に失敗しました。</p>';
     }
 
-
-    // 簡易的に、ページ離脱時にログアウト処理をする
-    if( isset($_COOKIE[session_name()]) ){
-      if($_SERVER['HTTP_HOST'] !== 'localhost.chat.test'){
-
-        $delete_id = null;
-
-        $statement = $pdo->prepare('DELETE FROM `login_user` WHERE `random_id` = :random_id');
-
-        $delete_id = (string) $_SESSION['data']['random_id'];
-
-        $statement->bindValue(':random_id', $delete_id, PDO::PARAM_STR);
-        $statement->execute();
-
-        $_SESSION = [];
-
-        setcookie(session_name(), '', time() - 36000);
-        session_destroy();
-
-        exit;
-      }
-    }
-
     ?>
