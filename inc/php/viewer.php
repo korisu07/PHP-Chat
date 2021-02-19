3a7e33c383751dbdb184be08c0bc4a8db14e4da6
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-19 19:28:27 +0900

    表示機能をクラス化しました

--- inc/php/viewer.php
+++ inc/php/viewer.php
@@ -6,29 +6,25 @@ class Viewer{
   protected array $logs = [];
 
   // construnct
-  public function __construct( array $logs )
+  public function __construct()
   {
-    $this->logs = $logs;
-  } //end func __construct
+  } //end func __construct.
 
   // HTMLタグをエスケープする
   private function htmlEscape( string $log_str ): string
   {
     return htmlspecialchars(strval($log_str), ENT_QUOTES | ENT_HTML5, 'UTF-8');
-  }
+  } //end func htmlEscape.
 
   // logを表示する
-  public function logSheet(): void
+  public function logSheet( array $logs ): void
   {
-    $log_array = $this->logs;
-    for ($i = 0; $i < count( $log_array, COUNT_RECURSIVE); $i++)
-    {
-      echo '<ul>';
-      echo '<li>', $this->htmlEscape( $log_array['user_name'] ), 'さんの発言：</li>';
-      echo '<li>', $this->htmlEscape( $log_array['message'] ), '</li>';
-      echo '<li>', $this->htmlEscape( $log_array['date'] ), '</li>';
-      echo '</ul>';
-    } //end for.
-  } //end func logSheet
+    $this->logs = $logs;
+    echo '<ul>';
+    echo '<li>', $this->htmlEscape( $this->logs['user_name'] ), 'さんの発言：</li>';
+    echo '<li>', $this->htmlEscape( $this->logs['message'] ), '</li>';
+    echo '<li>', $this->htmlEscape( $this->logs['date'] ), '</li>';
+    echo '</ul>';
+  } //end func logSheet.
 
 } //end class Viewer.
