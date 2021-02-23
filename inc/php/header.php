5399ec678f98124dd5607cf573eadfe6cc9feab8
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-23 15:44:19 +0900

    headerのテキストボックス表示の処理をクラス化して整理しました

--- inc/php/header.php
+++ inc/php/header.php
@@ -1,24 +1,43 @@
+<?php 
 
-  <?php include dirname(__FILE__) . '/connect/connect.php'; ?>
+class header {
 
-  <?php 
+  // 入室していないときに表示するログインフォーム
+  private string $loginForm;
 
-    // 名前が登録されている場合
-    if( isset($_SESSION['data']['name']) && $_SESSION['data']['name'] != '' ){
-      echo '
-      <form action="./routing/user_message_routing.php" method="post" class="chat_post">
-      <div class="header_box">
-        <label for="chat_message">メッセージ：</label>
-        <input type="text" name="chat_message" id="chat" maxlength="200">
-      </div>
+  // 入室しているときに表示される発言フォーム
+  private string $chatForm;
 
-      <input type="submit" value="発言する">
-      </form>
-      ';
-    }
-    // 名前が登録されていない場合
-    else{
-      echo '
+  // 入室しているときに表示されるユーザー自身の名前
+  private string $showUserName;
+
+  // ログイン中のユーザー名
+  private string $setUserName;
+
+  // 入室者一覧
+  private string $userList;
+
+  // ユーザー名が設定されているかをチェック
+  // trueなら設定されている状態。初期状態はfalse
+  private bool $checkSetUserName = false;
+
+  // construnct
+  public function __construct( $userName = null ){
+
+    // ユーザー名が設定されていないとき
+    if( is_null( $userName ) || $userName === '' ){
+
+      $this->checkSetUserName = false;
+
+    } else { // ユーザー名が設定されているとき 
+
+      $this->checkSetUserName = true;
+      $this->setUserName = $userName;
+
+    } // end if userName.
+
+    // ログインするためのフォーム
+    $this->loginForm = '
       <form action="./routing/user_name_routing.php" method="post" class="chat_post">
       <div class="header_box">
         <label for="user_name">ユーザー名：</label>
@@ -27,45 +46,53 @@
 
       <input type="submit" value="入室する">
       </form>
-      ';
-    }
-    
-    ?>
+    ';
 
-  <div class="chat_user">
+    // チャットで発言するためのフォーム
+    $this->chatForm = '
+      <form action="./routing/user_message_routing.php" method="post" class="chat_post">
+      <div class="header_box">
+        <label for="chat_message">メッセージ：</label>
+        <input type="text" name="chat_message" id="chat" maxlength="200">
+      </div>
 
-    <?php 
+      <input type="submit" value="発言する">
+      </form>
+    ';
 
-    // if(isset($_SESSION)){
-    //   $this->session->all_userdata();
-    // }
+    if( $this->checkSetUserName ){
+      // 入室中であるときに表示されるユーザー名
+      $this->showUserName = '
 
-    // try{
-    //   // ログイン中のユーザーを表示
-    //   $user_list = $pdo->prepare('SELECT login_user FROM login_user');
-    //   $user_list->execute();
-    // } catch(PDOException $e){
-    //   echo 'ユーザー 一覧の表示に失敗しました。';
-    // }
-
-    ?>
-
-    <!-- <span>入室中：</span> -->
-    <!-- < ? php // while ($log_value = $user_list->fetch(PDO::FETCH_ASSOC)): ?> -->
-      <!-- <span>< ? = text_escape($log_value['login_user']);?> さん ★ </span> -->
-    <!-- < ? php  // endwhile; ?> -->
-
-    <?php
-      if( isset($_SESSION['data']['name']) && $_SESSION['data']['name'] != '' ){
-
-        echo '<p>' , htmlspecialchars($_SESSION['data']['name']) , '　さんとして入室中</p>' ,
-      
+        <p>' . htmlspecialchars( $this->setUserName ) . '　さんとして入室中</p>' .
+          
         '<form action="./routing/chat_exit.php" method="post">
           <input type="submit" name="chat_exit" id="chat_exit" value="退室する">
-        </form>';
-      }
-  ?>
-  
+        </form>
+      </div>';
+    } // end if userList.
 
-  </div>
-</div>
+  }
+
+  public function loadTextarea():void
+  {
+    // ログイン中の場合に表示
+    if( $this->checkSetUserName ){
+
+      echo $this->chatForm;
+
+      echo '<div class="chat_user">';
+      echo $this->showUserName;
+      // ここに入室者一覧の処理
+      // echo $this->userList
+      echo '</div>';
+
+    } else { // ログアウト時に表示
+
+      echo $this->loginForm;
+
+    } // end if echoForm.
+
+  } // end func loadTextarea.
+
+} // end class header.
