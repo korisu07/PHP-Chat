a0bfb944dbab3d5ce3e67584d723998b1ffc14c1
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-23 15:44:14 +0900

    first commit.参考用に使っていない処理を分離

---
+++ inc/php/loginUserList.php
@@ -0,0 +1,22 @@
+<?php 
+// この機能は現在読み込まれていません。
+// 後に機能追加するときの参考用に、残してあります。
+
+// if(isset($_SESSION)){
+//   $this->session->all_userdata();
+// }
+
+// try{
+//   // ログイン中のユーザーを表示
+//   $user_list = $pdo->prepare('SELECT login_user FROM login_user');
+//   $user_list->execute();
+// } catch(PDOException $e){
+//   echo 'ユーザー 一覧の表示に失敗しました。';
+// }
+
+?>
+
+<!-- <span>入室中：</span> -->
+<!-- < ? php // while ($log_value = $user_list->fetch(PDO::FETCH_ASSOC)): ?> -->
+  <!-- <span>< ? = text_escape($log_value['login_user']);?> さん ★ </span> -->
+<!-- < ? php  // endwhile; ?> -->
