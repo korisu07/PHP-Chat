fcedff0e65023022a0fef45efa07f373f179752b
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-25 20:11:46 +0900

    first commit. チャット内に入室する処理を行うクラスを作成しました。

---
+++ inc/php/accessRouting/postMethod/loginChat.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+
+namespace accessRouting\postMethod;
+
+require_once dirname(__FILE__) . '/../setSession/setSession.php';
+require_once dirname(__FILE__) . '/interface/postMethod.php';
+
+
+class loginChat extends setSession implements postMethod
+{
+  // セッションをセットする
+  public function setSession()
+  {
+    
+  }
+
+  // SQLに接続する
+  public function connectSQL()
+  {
+
+  }
+
+  // 表示したいログをSQLに登録する
+  public function sendChatLog()
+  {
+
+  }
+}
