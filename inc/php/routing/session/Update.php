be4811c31d4c3528de82ad47574ec9de5f202b76
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-26 12:59:57 +0900

    セッション更新の処理を分割しました。

--- inc/php/routing/session/Update.php
+++ inc/php/routing/session/Update.php
@@ -4,6 +4,40 @@ namespace Routing\Session;
 
 require_once dirname(__FILE__) . '/Main.php';
 
-abstract class Expansion extends Routing\Session\Main{
+// ログイン時のセッション更新と、タイムスタンプ更新の処理を行うクラス
+abstract class Update extends Routing\Session\Main{
+
+  // 入室するときにセッションにユーザー名をセットする
+  public function setLoginSession(string $name, int $time):void
+  {
+    $this->timeStamp = date('Y-m-d G:i:s', $time);
+
+    // ランダムIDが必要になった場合、ここに関数を設定
+    $random_id = '';
+
+    $this->customSession = [
+      'name' => $name,
+      'random_id' => $random_id,
+      'time_stamp' => $this->timeStamp,
+      'error_message' => ''
+    ];
+
+    $_SESSION['data'] = $this->customSession;
+  } //end func loginSessionRouting.
+
+  // タイムスタンプの記録のみを更新する関数
+  public function updateTimeStampSession(int $time):void
+  {
+    $this->timeStamp = date('Y-m-d G:i:s', $time);
+
+    $this->customSession = [
+      'name' => $this->customSession['name'],
+      'random_id' => $this->customSession['random_id'],
+      'time_stamp' => $this->timeStamp,
+      'error_message' => ''
+    ];
+
+    $_SESSION['data'] = $this->customSession;
+  } //end func setTimeStampSession.
 
 }
