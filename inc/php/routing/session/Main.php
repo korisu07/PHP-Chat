ccfdc3b2a53ac1f16d2c443b85238aba51b5a4d8
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-26 12:59:43 +0900

    初期設定と基本機能のみに変更。その他の内容は分割しました。

--- inc/php/routing/session/Main.php
+++ inc/php/routing/session/Main.php
@@ -2,13 +2,17 @@
 
 namespace Routing\Session;
 
+// セッションの初期設定と基本機能を実装するクラス
 abstract class Main{
   
-  private string $timeStamp;
+  // タイムスタンプ
+  protected string $timeStamp;
 
+  // 初期設定されるセッション
   private array $firstSession;
 
-  private array $customSession;
+  // 後から更新されるセッション
+  protected array $customSession;
 
   // __construct
   public function __construct(int $time){ // インスタンス化するときに $_SERVER['REQUEST_TIME']を指定する
@@ -33,41 +37,13 @@ abstract class Main{
   } //end func setFirstSession.
 
   // エラーメッセージをセッション内に設定
-  public function errorMessage(string $message):void
+  public function setErrorMessage(string $message):void
   {
     $_SESSION['data']['error_message'] = $message;
   } //end func errorMessage.
 
-  // 入室するときにセッションにユーザー名をセットする
-  public function loginSessionRouting(string $name, int $time):void
-  {
-    $this->timeStamp = date('Y-m-d G:i:s', $time);
+  abstract function setLoginSession();
 
-    // ランダムIDが必要になった場合、ここに関数を設定
-    $random_id = '';
+  abstract function updateTimeStampSession();
 
-    $this->customSession = [
-      'name' => $name,
-      'random_id' => $random_id,
-      'time_stamp' => $this->timeStamp,
-      'error_message' => ''
-    ];
-
-    $_SESSION['data'] = $this->customSession;
-  } //end func loginSessionRouting.
-
-  // タイムスタンプの記録のみを更新する関数
-  public function setTimeStampSession(int $time):void
-  {
-    $this->timeStamp = date('Y-m-d G:i:s', $time);
-
-    $this->customSession = [
-      'name' => $this->customSession['name'],
-      'random_id' => $this->customSession['random_id'],
-      'time_stamp' => $this->timeStamp,
-      'error_message' => ''
-    ];
-
-    $_SESSION['data'] = $this->customSession;
-  } //end func setTimeStampSession.
 }
