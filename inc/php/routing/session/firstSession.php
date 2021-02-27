86257824018546ed9ad2868369b4ab1d55fb35ab
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-27 18:50:55 +0900

    first commit. セッション初期化の処理を行うクラスを作成。

---
+++ inc/php/routing/session/firstSession.php
@@ -0,0 +1,23 @@
+<?php declare(strict_types=1);
+
+namespace Routing\Session;
+
+require_once dirname(__FILE__) . '/Main.php';
+
+class FirstSession extends Main{
+  // 初回アクセス時のセッションを設定
+  public function setFirstSession():void
+  {
+    $this->timeStamp = date('Y-m-d G:i:s', $_SERVER['REQUEST_TIME']);
+
+    // セッションの初期設定
+    $this->firstSession = [
+      'name' => '',
+      'random_id' => '',
+      'time_stamp' => $this->timeStamp,
+      'error_message' => ''
+    ];
+
+    $_SESSION['data'] = $this->firstSession;
+  } //end func setFirstSession.
+}
