c0ffef9439262df9a4dde913e608d06705f372f1
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-27 18:54:05 +0900

    チャット入室の処理を試し、機能することを確認しました。

--- test.php
+++ test.php
@@ -1,19 +1,44 @@
-<?php 
+<?php declare(strict_types=1);
 
+require_once dirname(__FILE__) . '/inc/php/routing/session/firstSession.php';
 require_once dirname(__FILE__) . '/inc/php/routing/post/LoginChat.php';
 require_once dirname(__FILE__) . '/inc/php/routing/trait/CheckWord.php';
 
 use Routing\Post\LoginChat;
+use Routing\Session\FirstSession;
 use RoutingTrait\CheckWord;
 
-$userName = 'user_name01';
+if( is_null($_SESSION['data']) ){
+  $firstSession = new FirstSession();
+  $firstSession->setFirstSession();
+} //end if.
 
-$checkWord = new CheckWord();
-$bool = $checkWord->checkBool($userName, $ng_words, 'system');
+$_SESSION['data']['name'] = ' ';
 
-echo '$userNameの判定結果は', var_dump( $bool ), 'です。';
+$userName = $_SESSION['data']['name'];
 
-// $loginChat = new LoginChat( $bool );
-// $loginChat->setSession('user01', $_SERVER['REQUEST_TIME']);
+$checkWord = new CheckWord($userName, $ng_words, 'system');
+// セッションが設定されていない場合に、初期のセッションを設定
 
-// $loginChat->sendChatLog('user01');
+$bool = $checkWord->checkBool();
+
+$loginChat = new LoginChat( $bool, $_SERVER['REQUEST_TIME']);
+$loginChat->setSession($userName);
+
+$loginChat->sendChatLog($userName, $pdo);
+
+echo '$userNameの判定結果は';
+if( $bool ){
+  echo '「登録可能」';
+} else if ( !$bool ) {
+  echo '「登録不可」';
+}
+echo 'です。';
+
+if( isset($_SESSION['data']['error_message']) && $_SESSION['data']['error_message'] !== '' ){
+  echo $_SESSION['data']['error_message'];
+}
+
+if( isset($_SESSION['data']['name']) ){
+  echo '名前は：', htmlspecialchars($_SESSION['data']['name']), 'です。';
+}
