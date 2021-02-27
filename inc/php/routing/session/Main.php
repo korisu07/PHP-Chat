50178ca6d1c888403894f23a4c769a580cfac631
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-27 18:50:34 +0900

    インスタンス化できないことで不都合が起こったため、セッション初期化の処理を別クラスに移動

--- inc/php/routing/session/Main.php
+++ inc/php/routing/session/Main.php
@@ -2,6 +2,9 @@
 
 namespace Routing\Session;
 
+// セッションをスタート
+session_start();
+
 // セッションの初期設定と基本機能を実装するクラス
 abstract class Main{
   
@@ -14,26 +17,11 @@ abstract class Main{
   // 後から更新されるセッション
   protected array $customSession;
 
-  // 初回アクセス時のセッションを設定
-  public function setFirstSession(int $time):void  // 引数には $_SERVER['REQUEST_TIME'] を指定する
-  {
-    $this->timeStamp = date('Y-m-d G:i:s', $time);
-
-    // セッションの初期設定
-    $this->firstSession = [
-      'name' => '',
-      'random_id' => '',
-      'time_stamp' => $this->timeStamp,
-      'error_message' => ''
-    ];
-
-    $_SESSION['data'] = $this->firstSession;
-  } //end func setFirstSession.
-
   // エラーメッセージをセッション内に設定
   public function setErrorMessage(string $message):void
   {
     $_SESSION['data']['error_message'] = $message;
+    
   } //end func errorMessage.
 
 }
