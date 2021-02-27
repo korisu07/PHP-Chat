d0e9ad28227254981441da8726e8c43c1aaa6f02
korisu07 <Hallo.Dreamy@gmail.com>
2021-02-27 18:53:00 +0900

    connectSQL()をインターフェースから一旦削除（必要なら後に復活）また、関数に型を設定。

--- inc/php/routing/post/interface/PostMethod.php
+++ inc/php/routing/post/interface/PostMethod.php
@@ -7,11 +7,8 @@ require_once dirname(__FILE__) . '/../../../connect/connect.php';
 // POSTリクエストを行うクラスのインターフェース（最低限、実装するべき機能）
 interface PostMethod{
   // セッションをセットする
-  public function setSession(string $name, int $time);
-
-  // SQLに接続する
-  public function connectSQL();
+  public function setSession(string $name):void;
 
   // 表示したいログをSQLに登録する
-  public function sendChatLog(string $str);
+  public function sendChatLog(string $str):void;
 }
