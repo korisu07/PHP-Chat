a64c080965209fabe0676bef652ac20899b007ff
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-01 18:05:31 +0900

    first commit. 後々に更新がしやすいように静的なフォームパーツを別ファイルに移動しました

---
+++ inc/php/view/inc_header/chatForm.php
@@ -0,0 +1,8 @@
+<form action="./routing/user_message_routing.php" method="post" class="chat_post">
+  <div class="header_box">
+    <label for="chat_message">メッセージ：</label>
+    <input type="text" name="chat_message" id="chat" maxlength="200">
+  </div>
+
+  <input type="submit" value="発言する">
+</form>
---
+++ inc/php/view/inc_header/loginForm.php
@@ -0,0 +1,8 @@
+<form action="./routing/user_name_routing.php" method="post" class="chat_post">
+  <div class="header_box">
+    <label for="user_name">ユーザー名：</label>
+    <input type="text" name="user_name" id="chat" maxlength="20">
+  </div>
+
+  <input type="submit" value="入室する">
+</form>
