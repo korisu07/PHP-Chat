cd6bf1155ca7d57a4f682ea73d3b6f01e9a56745
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-02 18:06:24 +0900

    require_onceの順番等を整理しました

--- test.php
+++ test.php
@@ -1,12 +1,12 @@
 <?php declare(strict_types=1);
 
 require_once dirname(__FILE__) . '/inc/php/routing/session/firstSession.php';
-require_once dirname(__FILE__) . '/inc/php/routing/post/LoginChat.php';
 require_once dirname(__FILE__) . '/inc/php/routing/trait/CheckWord.php';
+require_once dirname(__FILE__) . '/inc/php/routing/post/LoginChat.php';
 
-use Routing\Post\LoginChat;
 use Routing\Session\FirstSession;
 use RoutingTrait\CheckWord;
+use Routing\Post\LoginChat;
 
 // セッションが設定されていない場合に、初期のセッションを設定
 if( is_null($_SESSION['data']) ){
@@ -18,7 +18,7 @@ if( is_null($_SESSION['data']) ){
 
 // 名前登録のテスト用
 // 本来は、ここに$_POST['user_name']の値が入る
-$userName = '';
+$userName = 'test_user';
 
 ///////////////////////////////////////////////////////
 
@@ -29,11 +29,11 @@ $checkWord = new CheckWord($userName, $ng_words, 'system');
 $bool = $checkWord->checkBool();
 
 // ログイン処理
-$loginChat = new LoginChat( $bool, $_SERVER['REQUEST_TIME']);
+$loginChat = new LoginChat( $bool, $_SERVER['REQUEST_TIME'] );
 // ユーザー名をセッションにセット
 $loginChat->setSession( $userName );
 // SQLへの送信
-$loginChat->sendChatLog($userName, $pdo);
+$loginChat->sendChatLog( $userName, $pdo );
 
 // $userNameの判定チェック
 echo '$userNameの判定結果は';
