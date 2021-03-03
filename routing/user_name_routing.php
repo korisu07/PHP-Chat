0651c822ad70ae7365f34753fc49ef14dea7dfd6
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-03 18:58:22 +0900

    チャットの入室処理をクラス化し、整理しました。

--- routing/user_name_routing.php
+++ routing/user_name_routing.php
@@ -1,62 +1,36 @@
-<?php
-  header('Location: /', 307);
-  session_start();
-  
-  require_once dirname(__FILE__) . '/../inc/php/function.php';
+<?php declare(strict_types=1);
+header('Location: /');
 
-  include dirname(__FILE__) . '/../inc/php/connect/connect.php';
-  include dirname(__FILE__) . '/../inc/php/connect/ng_word.php';
+require_once dirname(__FILE__) . '/../inc/php/routing/trait/CheckWord.php';
+require_once dirname(__FILE__) . '/../inc/php/routing/post/LoginChat.php';
 
-  // 未入力の場合
-  if(is_null($_POST['user_name']) || $_POST['user_name'] === ''){
-    $_SESSION['data']['error_message'] = 'ユーザー名を入力してください';
-    return false;
-    exit;
-  }else{
-  
-  //名前が入力された場合
+use RoutingTrait\CheckWord;
+use Routing\Post\LoginChat;
 
-    // NGワードチェック
-    $name_str = $_POST['user_name'];
+///////////////////////////////////////////////////////
 
-    // 大文字を小文字に変換
-    $name_str = mb_strtolower($name_str, 'UTF-8');
-    // 数字を半角に、半角カタカナは全角に変換
-    $name_str = mb_convert_kana($name_str, 'KVas', 'UTF-8');
-    // スペース、句読点などを削除
-    $target_sentence = preg_replace('/\s|、|。/', '', $name_str);
+// ユーザー名
+$userName = $_POST['user_name'];
 
-    foreach ($ng_words as $ngWordsVal) {
-      // 対象文字列にキーワードが含まれるか
-      if (mb_strpos($target_sentence, $ngWordsVal) !== FALSE) {
-        // 含まれている場合は処理を停止
-        $_SESSION['data']['error_message'] = '禁止ワードが含まれています。';
-        return false;
-        exit;
-      }
-    }
+///////////////////////////////////////////////////////
 
-    $login_user = null;
-    $random_id = null;
+// NGワードが含まれていないかをチェック
+$checkWord = new CheckWord($userName, $ng_words, 'system');
 
-    $login_user = (string) $_POST['user_name'];
-    $random_id = substr(str_shuffle('1234567890abcdefghijklmnopqrstuvwxyz'), 0, 10);
+// 返り値がtrueなら投稿可能
+$bool = $checkWord->checkBool();
 
-    $statement = $pdo->prepare('INSERT INTO chat_logs(`user_name`, `message`) VALUES(:system_name, :log_message)');
+///////////////////////////////////////////////////////
 
-    $log_message = null;
-    $log_message = $login_user . 'さんが入室しました。';
+// ログイン処理
+$loginChat = new LoginChat( $bool, $_SERVER['REQUEST_TIME'] );
+// ユーザー名をセッションにセット
+$loginChat->setSession( $userName );
 
-    $statement->bindValue(':system_name', 'system', PDO::PARAM_STR);
-    $statement->bindValue(':log_message', $log_message, PDO::PARAM_STR);
+// ここに連打判定の処理を追加
+$accessBool = null;
 
-    $statement->execute();
+// SQLへの送信
+$loginChat->sendChatLog( $userName, $pdo );
 
-
-    $_SESSION['data']['name'] = $login_user;
-    $_SESSION['data']['random_id'] = $random_id;
-    $_SESSION['data']['time_stamp'] = $_SERVER['REQUEST_TIME'];
-    $_SESSION['data']['error_message'] = '';
-    
-    exit;
-  }
+///////////////////////////////////////////////////////
