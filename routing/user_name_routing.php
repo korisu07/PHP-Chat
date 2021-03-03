db2d941253f6b8bf24809ffec571871102de7cbe
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-03 20:02:00 +0900

    連投していないかを判定する処理を作成中

--- routing/user_name_routing.php
+++ routing/user_name_routing.php
@@ -18,19 +18,25 @@ $userName = $_POST['user_name'];
 $checkWord = new CheckWord($userName, $ng_words, 'system');
 
 // 返り値がtrueなら投稿可能
-$bool = $checkWord->checkBool();
+$boolWord = $checkWord->checkBool();
 
 ///////////////////////////////////////////////////////
 
 // ログイン処理
-$loginChat = new LoginChat( $bool, $_SERVER['REQUEST_TIME'] );
-// ユーザー名をセッションにセット
-$loginChat->setSession( $userName );
+$loginChat = new LoginChat( $boolWord, $_SERVER['REQUEST_TIME'] );
+// 前回の通信リクエストから1秒経過しているかを判定
+$boolReload = $checkReload->JudgeRepeatedHits( $_SERVER['REQUEST_TIME'] );
 
-// ここに連打判定の処理を追加
-$accessBool = null;
+if( $boolWord && $boolReload )
+{
+  // ユーザー名をセッションにセット
+  $loginChat->setSession( $userName );
+  // SQLへの送信
+  $loginChat->sendChatLog( $userName, $pdo );
 
-// SQLへの送信
-$loginChat->sendChatLog( $userName, $pdo );
+} else {
+  $checkReload->setErrorMessage('連投はできません。少々お待ち下さい。');
+  echo '連投はできません。少々お待ち下さい。';
+}
 
 ///////////////////////////////////////////////////////
