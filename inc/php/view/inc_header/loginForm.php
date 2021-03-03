55d14328abe1c748876568aa7f35249aaa0cbffa
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-03 18:47:33 +0900

    formタグにID属性を追加

--- inc/php/view/inc_header/loginForm.php
+++ inc/php/view/inc_header/loginForm.php
@@ -1,8 +1,8 @@
-<form action="./routing/user_name_routing.php" method="post" class="chat_post">
+<form action="./routing/user_name_routing.php" method="post" class="chat_post" id="user_name">
   <div class="header_box">
     <label for="user_name">ユーザー名：</label>
-    <input type="text" name="user_name" id="chat" maxlength="20">
+    <input type="text" name="user_name" id="chat" maxlength="20" form="user_name">
   </div>
 
-  <input type="submit" value="入室する">
+  <input type="submit" value="入室する" for="user_name" form="user_name">
 </form>
