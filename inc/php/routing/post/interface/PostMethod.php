9bbe4daef64fbb02de0ca03b9b7317eb0dfd9ac0
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-04 17:07:34 +0900

    インターフェースからセッションの指定を取り除きました…

--- inc/php/routing/post/interface/PostMethod.php
+++ inc/php/routing/post/interface/PostMethod.php
@@ -6,8 +6,6 @@ require_once dirname(__FILE__) . '/../../../connect/connect.php';
 
 // POSTリクエストを行うクラスのインターフェース（最低限、実装するべき機能）
 interface PostMethod{
-  // セッションをセットする
-  public function setSession(string $str):void;
 
   // 表示したいログをSQLに登録する
   public function sendChatLog(string $str, $pdo):void;
