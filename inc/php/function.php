48d350dd9116d84efbfe4f85962951cf46572c35
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-05 17:26:20 +0900

    不要になったため削除

--- inc/php/function.php
+++
@@ -1,7 +0,0 @@
-<?php 
-  // ログ等をエスケープしてページ上に表示するための関数
-  // function text_escape($text_value){
-  //   return htmlspecialchars(strval($text_value), ENT_QUOTES | ENT_HTML5, 'UTF-8');
-  // }
-
-  $reload_time_stamp = $_SERVER['REQUEST_TIME'];
