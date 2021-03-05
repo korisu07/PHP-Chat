bb8da5e7a3b6595b8d758d5683243c37636729ee
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-05 17:56:09 +0900

    不要となったため削除

--- inc/php/view/inc_header/chatForm.php
+++
@@ -1,8 +0,0 @@
-<form action="./routing/user_message_routing.php" method="post" class="chat_post" id="chat_message">
-  <div class="header_box">
-    <label for="chat_message">メッセージ：</label>
-    <input type="text" name="chat_message" id="chat" maxlength="200" form="chat_message">
-  </div>
-
-  <input type="submit" value="発言する" for="chat_message" form="chat_message">
-</form>
