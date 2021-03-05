aa5040f6c4cdbb2f152779ee986f44159c06a4e5
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-05 18:35:49 +0900

    「チャットアプリ」に名称変更

--- index.php
+++ index.php
@@ -10,7 +10,7 @@ require_once dirname(__FILE__) . '/inc/php/view/header.php';
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>チャットツール</title>
+  <title>チャットアプリ</title>
 
   <link rel="stylesheet" href="./inc/css/style.min.css">
 </head>
