f1fb2df31ebdb43440c6277e7c861441ca5553dd
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-05 17:27:48 +0900

    読み込み部分の移動や削除をし、記述を整理しました。

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php 
 header("Content-type: text/html; charset=utf-8");
+
+require_once dirname(__FILE__) . '/inc/php/view/viewer.php';
+require_once dirname(__FILE__) . '/inc/php/view/header.php';
+
  ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -11,65 +15,54 @@ header("Content-type: text/html; charset=utf-8");
   <link rel="stylesheet" href="./inc/css/style.min.css">
 </head>
 <body>
-
-  <?php 
-    require_once dirname(__FILE__) . '/inc/php/function.php';
-
-    require_once dirname(__FILE__) . '/inc/php/connect/connect.php';
-
-    require_once dirname(__FILE__) . '/inc/php/view/viewer.php';
-
-  ?>
-
   <header>
-    
   <?php 
+    // エラーメッセージが設定されている場合、表示する
     if(isset($_SESSION['data']['error_message']) && $_SESSION['data']['error_message'] != ''){
+      // エラーメッセージを表示
       echo '<div class="error_message"><div class="container">' , 
       $_SESSION['data']['error_message'] ,
       '</div></div>';
-    }
-  ?>
+    } //end if, Error message.
+  ?><!-- PHP -->
 
-    <div class="container">
-
-      <?php require_once dirname(__FILE__) . '/inc/php/view/header.php';
-        $header = new header();
-        $header->loadTextarea();
-      ?>
-      
-    </div>
+  <div class="container">
+    <?php // header内のテキストボックスを表示
+      $header = new header();
+      $header->loadTextarea();
+    ?><!-- PHP -->
+  </div>
   </header>
   
   <article>
     <div class="container">
-      
       <div class="chat_logs_view">
-
       <?php 
         // チャットログを表示
         try{
+          // SQLへ接続
           $access_process = $pdo->prepare('SELECT * FROM chat_logs ORDER BY id DESC LIMIT 20');
+          // 接続処理を実行
           $access_process->execute();
-
         } catch(PDOException $e){
+          // 接続に失敗した場合
           echo '<p>チャットログの表示に失敗しました。</p>';
-        }
+        } //end try~catch.
 
+        // SQLのログを読み込み
         while( $sql_log_data = $access_process->fetch(PDO::FETCH_ASSOC) ){
           $viewer = new Viewer( $sql_log_data );
           $viewer->logSheet();
-        }
-      ?>
+        }//end while.
+      ?><!-- PHP -->
       </div> <!-- /.chat_logs_view -->
-
-    </div>
+    </div> <!-- /.container -->
   </article>
 
   <footer>
     <div class="container">
-      chat.app 2020 
-    </div>
+      chat.app 2020 - <?= date('Y') ?>.
+    </div> <!-- /.container -->
   </footer>
 </body>
 </html>
