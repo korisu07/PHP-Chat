55302d30ca38380ab97de1eee982be421c106da7
korisu07 <Hallo.Dreamy@gmail.com>
2021-03-17 20:40:22 +0900

    エラーメッセージを修正

--- routing/user_name_routing.php
+++ routing/user_name_routing.php
@@ -32,7 +32,7 @@ $loginChat->sendChatLog( $userName, $pdo );
 // 連投されていた場合
 if( $boolReload === false && $_SESSION['data']['error_message'] === '' ) {
   // セッションにエラーメッセージをセット
-  $checkReload->setErrorMessage('連投はできません。少々お待ち下さい。');
+  $checkReload->setErrorMessage('時間をあけてから入室してください。');
 }
 
 
